ee01010d81c8341afd84d4394adc58ab2adcb199
okandeji <okandeji2012@gmail.com>
2022-02-18 11:43:54 +0100

    Fix dashboard layout

--- container/layouts/dashboard.layout.tsx
+++ container/layouts/dashboard.layout.tsx
@@ -1,15 +1,12 @@
-import React, {useState, useEffect} from "react";
-import { Layout, Menu } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Button, Drawer, Layout, Menu, PageHeader, Tooltip } from "antd";
 import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
   BankOutlined,
   SettingOutlined,
-  StockOutlined,
   LogoutOutlined,
   ApartmentOutlined,
   TeamOutlined,
@@ -17,13 +14,19 @@ import {
   SoundOutlined,
   HomeOutlined,
   FundProjectionScreenOutlined,
-} from '@ant-design/icons';
+  MailOutlined,
+  PlusOutlined,
+  GlobalOutlined,
+} from "@ant-design/icons";
 import Link from "next/link";
-import Image from 'next/image'
-import {useRouter, Router} from "next/router";
-import {useSelector, useDispatch} from "react-redux";
-import {IOption} from '../../types/types';
+import Image from "next/image";
+import { Router, useRouter } from "next/router";
+import { useSelector, useDispatch } from "react-redux";
+import { IOption } from "../../types/types";
 import styled from "@emotion/styled";
+import { useTheme } from "@emotion/react";
+import ProfilePopup from "../../components/profile-popover.component";
+import ProfilePopover from "../../components/profile-popover.component";
 
 const { Header, Sider, Content } = Layout;
 
@@ -151,11 +154,11 @@ const AppMenu = () => {
           subMenu: [
             {
               title: "Profile",
-              link: "/real-estate/settings/profile"
+              link: "/real-estate/settings/profile",
             },
             {
               title: "Change Password",
-              },
+            },
             {
               title: "Configuration",
               link: "/users/settings/configuration",
@@ -230,11 +233,11 @@ const AppMenu = () => {
           subMenu: [
             {
               title: "Profile",
-              link: "/real-estate/settings/profile"
+              link: "/real-estate/settings/profile",
             },
             {
               title: "Change Password",
-              },
+            },
           ],
         },
         {
@@ -305,11 +308,11 @@ const AppMenu = () => {
           subMenu: [
             {
               title: "Profile",
-              link: "/real-estate/settings/profile"
+              link: "/real-estate/settings/profile",
             },
             {
               title: "Change Password",
-              },
+            },
           ],
         },
         {
@@ -380,11 +383,11 @@ const AppMenu = () => {
           subMenu: [
             {
               title: "Profile",
-              link: "/real-estate/settings/profile"
+              link: "/real-estate/settings/profile",
             },
             {
               title: "Change Password",
-              },
+            },
           ],
         },
         {
@@ -431,11 +434,11 @@ const AppMenu = () => {
           subMenu: [
             {
               title: "Profile",
-              link: "/real-estate/settings/profile"
+              link: "/real-estate/settings/profile",
             },
             {
               title: "Change Password",
-              },
+            },
           ],
         },
         {
@@ -446,33 +449,30 @@ const AppMenu = () => {
     },
   ];
 
-  const setMenu = () =>
-    menus.find((item) => item.role === 'admin')?.routes;
+  const setMenu = () => menus.find((item) => item.role === "admin")?.routes;
 
   const menu = setMenu();
   return (
     <MenuStyled>
       {/* <Deposit /> */}
       <div className="m-4">
-        <img className="w-1 h-1" src="/images/logo.png" alt="septem logo"/>
+        <img className="menu w-10 md:w-1/5" src="/images/logo.png" alt="septem logo" />
       </div>
       <Menu
         theme="dark"
         mode="inline"
         defaultSelectedKeys={[router.pathname]}
-        className="menu"
-      >
+        className="menu">
         {
           // @ts-ignore
           menu?.map((item, ii) =>
-          // @ts-ignore
+            // @ts-ignore
             item.subMenu?.length > 0 ? (
               <Menu.SubMenu
                 title={item.title}
                 icon={item.icon}
                 key={item.title}
-                className="sub-menu"
-              >
+                className="sub-menu">
                 {
                   // @ts-ignore
                   item.subMenu.map((e, i) => (
@@ -500,8 +500,7 @@ const AppMenu = () => {
                 // onClick={() => {
                 //   item.action ? item.action() : handleClick(item.link);
                 // }}
-                className="sub"
-              >
+                className="sub">
                 {item.link ? (
                   <Link href={item.link} key={item.title}>
                     <a>{item.title}</a>
@@ -519,39 +518,144 @@ const AppMenu = () => {
 };
 
 const DashboardLayout: any = (props: IOption) => {
-  const {children} = props;
+  const { children } = props;
+  const theme: IOption = useTheme();
+  const screens = useBreakpoint();
   const [collapsed, setCollapsed] = useState(false);
+  const [visibleProfile, setVisibleProfile] = useState(false);
+  const router = useRouter();
+
+  const dispatch = useDispatch();
   const toggle = () => {
     setCollapsed((prevState) => !prevState);
   };
 
-//   const toggle = () => {
-//     setState({
-//       collapsed: !state.collapsed,
-//     });
-//   };
+  const userData = {
+    username: "Deji",
+    userId: "2356",
+    role: "admin",
+  };
 
-    return (
-      <DashboardStyled className="min-h-screen">
-        <Sider trigger={null} collapsible collapsed={collapsed} className="hidden md:block md:bg-primary">
-          <AppMenu />
-        </Sider>
-        <Layout className="bg-white">
-          <Header className="flex items-center justify-between px-12 py-2 text-white bg-primary lg:bg-tertiary lg:text-primary">
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'text-2xl leading-10 cursor-pointer transition-colors hover:text-lightPrimary',
+  return (
+    <DashboardStyled className="min-h-screen">
+      <ProfilePopup
+        visible={visibleProfile}
+        setVisible={setVisibleProfile}
+        width={!screens.lg ? "100%" : "30%"}
+        theUser={userData}
+        editUser={userData}
+      />
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className="hidden md:block md:bg-primary">
+        <AppMenu />
+      </Sider>
+      <Layout className="bg-white">
+        <Header className="flex items-center justify-between px-12 py-2 text-white bg-primary lg:bg-tertiary lg:text-primary">
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className:
+                "text-2xl leading-10 cursor-pointer transition-colors hover:text-lightPrimary",
               onClick: toggle,
-            })}
-          </Header>
-          <Content
-            className="p-4 lg:p-10 min-h-3/4"
-          >
-            {children}
-          </Content>
-        </Layout>
-      </DashboardStyled>
-    );
-}
+            }
+          )}
+          <span className="flex items-center justify-between">
+            {screens.lg ? (
+              <GlobalOutlined className="mx-2 text-2xl text-primary text-white" onClick={() => router.push('/')} />
+            ) : (
+              <GlobalOutlined className="mx-2 text-2xl text-white" onClick={() => router.push('/')} />
+            )}
+            {/* {!screens.xs && ( */}
+            {/* <Tooltip title="click to refresh">
+              <Button
+                htmlType="button"
+                onClick={refresh}
+                className="flex-col items-center justify-center flex-grow hidden h-12 px-4 mx-2 leading-6 btn-transparent md:flex "
+              >
+                {userData.data.role === "cashier" ||
+                userData.data.role === "online-customer" ? null : ( // Cashier and online user don't have access to trust
+                  <span className="text-white lg:text-primary">
+                    Trust:
+                    {userData.data.role !== "agent"
+                      ? // Only agent have -ve trust balance
+                        formatter.format(
+                          currencify(userData.data.wallet.trust, "/")
+                        )
+                      : userData?.data.wallet.trust === 0
+                      ? formatter.format(
+                          currencify(userData?.data.wallet.trust, "/")
+                        )
+                      : "-" +
+                        formatter.format(
+                          currencify(userData?.data.wallet.trust, "/")
+                        )}
+                  </span>
+                )}
+                <span className="text-white lg:text-primary">
+                  Bal:
+                  {formatter.format(
+                    currencify(userData.data.wallet.balance, "/")
+                  )}
+                </span>
+              </Button>
+            </Tooltip>
+            {/* )} */}
+            <ProfilePopover
+            // user={userData.data}
+            // logout={() => dispatch(setLogout())}
+            >
+              {screens.lg ? (
+                <UserOutlined className="mx-2 text-2xl text-primary" />
+              ) : (
+                <UserOutlined className="mx-2 text-2xl text-white" />
+              )}
+            </ProfilePopover>
+          </span>
+        </Header>
+        {/* {!screens.lg&& ( */}
+        <>
+          <Drawer
+            visible={collapsed}
+            placement="left"
+            closable={true}
+            onClose={() => setCollapsed(false)}
+            className="block md:hidden"
+            // closeIcon={<CloseOutlined className='trigger'/>}
+            bodyStyle={{
+              padding: 0,
+              background: theme.colors.primary,
+            }}>
+            <AppMenu />
+          </Drawer>
+          {/* {screens.xs && ( */}
+          <Tooltip title="click to refresh">
+            <Button
+              htmlType="button"
+              // onClick={refresh}
+              className="flex flex-col items-center w-full h-16 max-h-screen mb-3 leading-6 md:hidden bg-tertiary">
+              click me
+            </Button>
+          </Tooltip>
+          {/* )} */}
+        </>
+        {/* )} */}
+        {!router.pathname.includes("overview") ? (
+          <PageHeader
+            className="px-10 pt-2 pb-0"
+            onBack={() => router.back()}
+            title={
+              <div className="text-2xl font-normal text-gray-700">Back</div>
+            }
+          />
+        ) : null}
+        <Content className="p-4 lg:p-10 min-h-3/4">{children}</Content>
+      </Layout>
+    </DashboardStyled>
+  );
+};
 
 const DashboardStyled = styled(Layout)`
   .ant-menu-item-selected .ant-menu-item-selected a {
